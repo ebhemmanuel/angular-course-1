369f6a02c9d43ef7631cd1092aab235676c2191a
Emmanuel Barreto <Emmanuel.Barretto@universalorlando.com>
2023-12-07 00:28:28 -0500

    assigning branch for ngIf

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 
-
 @Component({
   selector: 'course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
+
 export class CourseCardComponent implements OnInit{
   @Input()
   course: Course;  
@@ -18,6 +18,11 @@ export class CourseCardComponent implements OnInit{
   courseSelected = new EventEmitter<Course>();
   ngOnInit(): void {
   }
+
+  isImageVisible(){
+    return this.course && this.course.iconUrl;
+  }
+
   onCourseViewed() {
     console.log('viewing the course');
     this.courseSelected.emit(this.course)
