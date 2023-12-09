9db1ab31086d391dfa96d7e051c4dfeccd026c73
Emmanuel Barreto <Emmanuel.Barretto@universalorlando.com>
2023-12-09 13:30:38 -0500

    viewchild query selection depth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -8,7 +8,7 @@ import { CourseCardComponent } from './course-card/course-card.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   // courses = [];
   courses = COURSES;
@@ -19,7 +19,15 @@ export class AppComponent {
   @ViewChild('container')
   containerDiv: ElementRef;
 
-  onCourseSelected(course:Course) {
+  constructor () {
+    
+  }
+  ngAfterViewInit() {
     console.log("containerDiv",this.containerDiv);
+   
+  }
+
+  onCourseSelected(course:Course) {
+    
   }
 }
